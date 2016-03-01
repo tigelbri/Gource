41ab09e466d6a2daa4beecf8eaec5a11dd748dd5
Tristan Igelbrink <Tristan@Igelbrink.com>
2016-03-01 16:39:13 +0100

    added title

--- gource_multi.sh
+++ gource_multi.sh
@@ -37,6 +37,7 @@ time gource $combined_log \
 	--key \
 	--stop-at-end \
         --font-size 18\
-        --user-font-size 27  
+        --user-font-size 27\
+        --title "My Project"  
 	#--output-ppm-stream - | ffmpeg -y  -r 60 -f image2pipe -vcodec ppm -i - -threads 0 -r 24000/1001 -b 6144k -bt 8192k -vcodec libx264 -pass 1 -flags +loop -me_method dia -g 250 -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -bf 16 -b_strategy 1 -i_qfactor 0.71 -cmp +chroma -subq 1 -me_range 16 -coder 1 -sc_threshold 40 -flags2 -bpyramid-wpred-mixed_refs-dct8x8+fastpskip -keyint_min 25 -refs 1 -trellis 0 -directpred 1 -partitions -parti8x8-parti4x4-partp8x8-partp4x4-partb8x8 -an $outfile
 rm $combined_log
